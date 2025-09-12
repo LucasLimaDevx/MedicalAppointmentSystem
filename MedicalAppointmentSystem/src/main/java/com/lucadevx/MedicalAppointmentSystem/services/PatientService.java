a8150c6ce7384e3d825e42c445756183f8c2a4c0
Lucas Lima <lucaslimadevx@gmail.com>
2025-09-12 17:09:41 -0300

    Add services package

---
+++ MedicalAppointmentSystem/src/main/java/com/lucadevx/MedicalAppointmentSystem/services/PatientService.java
@@ -0,0 +1,24 @@
+package com.lucadevx.MedicalAppointmentSystem.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucadevx.MedicalAppointmentSystem.model.Patient;
+import com.lucadevx.MedicalAppointmentSystem.repository.PatientRepository;
+
+@Service
+public class PatientService {
+	
+	@Autowired
+	private PatientRepository repository;
+	
+	public Patient create(Patient patient) {
+		
+		return repository.save(patient);
+	}
+	
+	public Patient findById(Long id) {
+		
+		return repository.findById(id).orElseThrow(()-> new IllegalArgumentException("Object not found"));
+	}
+}
