2c216afd389f27d0ffdf4503c97c4ec1842d189d
Lucas Lima <lucaslimadevx@gmail.com>
2025-10-21 21:42:40 -0300

    Create ObjectNotFoundException and ObjectIsNullException

---
+++ MedicalAppointmentSystem/src/main/java/com/lucadevx/MedicalAppointmentSystem/exception/ObjectIsNullException.java
@@ -0,0 +1,14 @@
+package com.lucadevx.MedicalAppointmentSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class ObjectIsNullException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectIsNullException(String message) {
+		super(message);
+	}
+}
---
+++ MedicalAppointmentSystem/src/main/java/com/lucadevx/MedicalAppointmentSystem/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.lucadevx.MedicalAppointmentSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+}
