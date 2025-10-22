ef93418cda42697b220568cbb1abeb79d72ba9cb
Lucas Lima <lucaslimadevx@gmail.com>
2025-10-21 23:55:36 -0300

    Create HandlerException

---
+++ MedicalAppointmentSystem/src/main/java/com/lucadevx/MedicalAppointmentSystem/exception/handler/HandlerException.java
@@ -0,0 +1,30 @@
+package com.lucadevx.MedicalAppointmentSystem.exception.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.lucadevx.MedicalAppointmentSystem.exception.ExceptionResponse;
+import com.lucadevx.MedicalAppointmentSystem.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class HandlerException {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ExceptionResponse> handleAllException(Exception exception , WebRequest request){
+		ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), exception.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> handleAllException(ObjectNotFoundException exception , WebRequest request){
+		ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), exception.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+}
